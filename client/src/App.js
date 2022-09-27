61eeaf5a64c89a654607fac65fab9bf219e27899
Korneli Nastas <korneli.nastas@gmail.com>
2022-09-27 14:32:04 +0200

    MainPage

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,8 @@
 import React from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import HomePage from "./Pages/HomePage";
+
+import MainPage from "./Pages/MainPage";
 //import { Routes, Route } from "react-router-dom";
 
 function App() {
@@ -9,7 +10,7 @@ function App() {
     <>
       <Header />
       <main className="py-3">
-        <h1>Wellcome</h1>
+        <MainPage />
       </main>
       <Footer />
     </>
