d83f9a1a25c6a064e1c067193b854c314379a9ab
Korneli Nastas <korneli.nastas@gmail.com>
2022-09-27 14:10:19 +0200

    some new changes

--- client/src/products.js
+++ client/src/products.js
@@ -14,7 +14,7 @@ const products = [
   },
   {
     _id: "2",
-    name: "iPhone 11 Pro 256GB Memory",
+    name: "iWomen's leather slippers Bear loves honey",
     image: "/images/2.jpg",
     description:
       "IA joint collaboration between rxbshoes and Andrey Bartenev, one of the most recognizable contemporary artists. It refers us to childish sincerity, to pure emotions and the joy of life that goes on, no matter what. This collection is beautiful and life-affirming. And the bears turned out to be not only childishly cute, but also mysterious: smiling slyly, they know something, but do not speak.oducing the iPhone 11 Pro. A transformative triple-camera system that adds tons of capability without complexity. An unprecedented leap in battery life",
@@ -27,7 +27,7 @@ const products = [
   },
   {
     _id: "3",
-    name: "Cannon EOS 80D DSLR Camera",
+    name: "Women's leather slippers Bear loves honey",
     image: "/images/3.jpg",
     description:
       "Silk is a pleasant and beautiful material, which is why we decided to use it for Minimi mules. And of course, we added a comfortable, small 5 cm glass heel for a more playful mood. Suitable for both home and outdoor walks. We advise you to choose a size up, as the narrow nose makes them a little smaller.",
@@ -40,7 +40,7 @@ const products = [
   },
   {
     _id: "4",
-    name: "Sony Playstation 4 Pro White Version",
+    name: "Women's leather slippers Bear loves honey",
     image: "/images/4.jpg",
     description:
       "The elegant shape of silk mules with a velvet heart-shaped nose will win your heart. We added a comfortable, small 5 cm glass heel for a more playful mood and a special cat walk. Suitable for both home and outdoor walks. We advise you to choose a size up, as the narrow nose makes them a little smaller.",
@@ -53,7 +53,7 @@ const products = [
   },
   {
     _id: "5",
-    name: "Logitech G-Series Gaming Mouse",
+    name: "Women's leather slippers Bear loves honey",
     image: "/images/5.jpg",
     description:
       "Especially for the 2022 Water Tiger, we have created a limited collection of indoor shoes with the tiger king, a powerful symbol of the year. The motifs of the painting by the famous artist K. Ovchinnikov were used in the design. They will bring you good luck in the coming year and will patiently wait for your return home. Take them with you on your travels and create the comfort of home wherever you are. And of course it's the perfect gift!",
@@ -66,7 +66,7 @@ const products = [
   },
   {
     _id: "6",
-    name: "Amazon Echo Dot 3rd Generation",
+    name: "Women's leather slippers Bear loves honey",
     image: "/images/6.jpg",
     description:
       "Cozy and soft house shoes will warm and cheer you up. Made from soft Italian leather, incredibly comfortable and weightless, silent, do not leave marks on the floor. Service life up to 5 years. This is your piece of art in the interior and a great gift.",
