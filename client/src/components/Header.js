56fabd9634d63da675f7f1efebf1dc549bf5ef9b
LenaGarilis <ma_diana@bk.ru>
2022-09-27 13:21:04 +0200

    style change

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -4,12 +4,15 @@ import { Navbar, Nav, Container, NavDropdown } from "react-bootstrap";
 const Header = () => {
   return (
     <header>
-      <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
+      <Navbar bg="primary" variant="dark" expand="lg" collapseOnSelect>
         <Container>
           <Navbar.Brand>InStyle</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="ml-auto">
+          <Navbar.Collapse
+            id="basic-navbar-nav"
+            className="justify-content-end"
+          >
+            <Nav>
               <Nav.Link href="/cart">
                 <i className="fas fa-shopping-cart"></i> Cart
               </Nav.Link>
