6f0d794991a68c1a10cd2060bbe430d607efab51
Korneli Nastas <korneli.nastas@gmail.com>
2022-10-18 10:05:41 +0200

    add footer

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Container } from "react-bootstrap";
 import Header from "./components/Header";
-import Footer from "./components/Footer";
+import Footer from "./components/footer/Footer";
 import HomeScreen from "./Screens/MainPage";
 import ProductScreen from "./Screens/ProductPage";
 import CartScreen from "./Screens/CartPage";
